b9253a54cbf5439c968c314db7cc1346e289ca02
Eduardoperez1010 <lalo383596@gmail.com>
2021-03-14 16:04:25 -0600

    Menu creado con javascript

--- js/menu.js
+++ js/menu.js
@@ -1,2 +1,17 @@
-const hamburguer s=document.querySelector(',hamburguer');
-const nebu s=document.querySelector('.menu-navegacion');
+const hamburguer = document.querySelector('.hamburguer');
+const menu = document.querySelector('.menu-navegacion');
+
+console.log(menu)
+console.log(hamburguer)
+
+
+hamburguer.addEventListener('click' , ()=>{
+    menu.classList.toggle("spread");
+})
+
+window.addEventListener('click', e=>{
+    console.log(e.target)
+    if (menu.classList.contains('spread') && e.target != menu && e.target != hamburguer) {
+        menu.classList.toggle("spread");
+    }
+}) 
